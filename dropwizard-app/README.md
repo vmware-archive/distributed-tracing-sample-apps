e845c2e6d50555234af8cae807a79b77b2cd0e08
Hao Song <mail@songhao.xyz>
2018-11-05 14:21:37 -0800

    Initial commit of dropwizard app

---
+++ dropwizard-app/README.md
@@ -0,0 +1,26 @@
+# Wavefront Hackthon - Dropwizard App
+
+This is a sample java application using Dropwizard beachshirts which makes cool shirts for beach. 
+
+## Running Application locally 
+
+- Run `mvn clean install` from the root directory of the project.
+
+- Now run all the services using below commands from root directory of the project.
+
+  ```bash
+  java -jar ./shopping/target/shopping-1.0.0-SNAPSHOT.jar server ./shopping/app.yaml
+  java -jar ./styling/target/styling-1.0.0-SNAPSHOT.jar server ./styling/app.yaml
+  java -jar ./delivery/target/delivery-1.0.0-SNAPSHOT.jar server ./delivery/app.yaml
+  ```
+
+- Now view the shopping menus using HTTP GET request: `http://localhost:50050/shop/menu`
+
+- Order shirts using HTTP POST request:
+
+- ```bash
+  http://localhost:50050/shop/order
+  Payload: {"styleName" : "testStyle1","quantity" : 5}
+  ```
+
+- Use `./loadgen {interval}` in the root directory to send a request of ordering shirts every  `{interval}` seconds
---
+++ dropwizard-app/delivery/src/main/java/com/wfsample/delivery/DeliveryService.java
@@ -0,0 +1,107 @@
+package com.wfsample.delivery;
+
+import com.wfsample.common.DropwizardServiceConfig;
+import com.wfsample.common.dto.PackedShirtsDTO;
+import com.wfsample.common.dto.DeliveryStatusDTO;
+import com.wfsample.service.DeliveryApi;
+
+import java.util.Queue;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+import javax.ws.rs.core.Response;
+
+import io.dropwizard.Application;
+import io.dropwizard.lifecycle.setup.ScheduledExecutorServiceBuilder;
+import io.dropwizard.setup.Environment;
+
+/**
+ * Driver for styling service which manages different styles of shirts and takes orders for a shirts
+ * of a given style.
+ *
+ * @author Srujan Narkedamalli (snarkedamall@wavefront.com).
+ */
+public class DeliveryService extends Application<DropwizardServiceConfig> {
+  /*
+   * TODO: Add a gauge to monitor the size of dispatch queue.
+   */
+  private static Queue<PackedShirtsDTO> dispatchQueue;
+
+  private DeliveryService() {
+  }
+
+  public static void main(String[] args) throws Exception {
+    new DeliveryService().run(args);
+  }
+
+  @Override
+  public void run(DropwizardServiceConfig configuration, Environment environment) {
+    dispatchQueue = new ConcurrentLinkedDeque<>();
+    environment.jersey().register(new DeliveryWebResource());
+    ScheduledExecutorServiceBuilder sesBuilder =
+        environment.lifecycle().scheduledExecutorService("Clear Queue");
+    ScheduledExecutorService ses = sesBuilder.build();
+    Runnable clearQueueTask = new ClearQueueTask();
+    ses.scheduleWithFixedDelay(clearQueueTask, 30, 30, TimeUnit.SECONDS);
+  }
+
+  private static final class ClearQueueTask implements Runnable {
+    @Override
+    public void run() {
+      System.out.println("Processing " + dispatchQueue.size() + " in the Dispatch Queue!");
+      while (!dispatchQueue.isEmpty()) {
+        deliverPackedShirts(dispatchQueue.poll());
+      }
+    }
+
+    private void deliverPackedShirts(PackedShirtsDTO packedShirtsDTO) {
+      /*
+       * TODO: Try to Increment a counter when one packed shirts delivered.
+       */
+      System.out.println(packedShirtsDTO.getShirts().size() + " shirts delivered!");
+    }
+  }
+
+  public class DeliveryWebResource implements DeliveryApi {
+
+    DeliveryWebResource() {
+    }
+
+    @Override
+    public Response dispatch(String orderNum, PackedShirtsDTO packedShirts) {
+      if (ThreadLocalRandom.current().nextInt(0, 5) == 0) {
+        return Response.status(Response.Status.SERVICE_UNAVAILABLE).entity("Failed to dispatch " +
+            "shirts!").build();
+      }
+      if (orderNum.isEmpty()) {
+        /*
+         * TODO: Try to emitting an error counter to Wavefront.
+         */
+        return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Order Num").build();
+      }
+      if (packedShirts == null || packedShirts.getShirts() == null ||
+          packedShirts.getShirts().size() == 0) {
+        /*
+         * TODO: Try to emitting an error counter to Wavefront.
+         */
+        return Response.status(Response.Status.BAD_REQUEST).entity("no shirts to deliver").build();
+      }
+      dispatchQueue.add(packedShirts);
+      String trackingNum = UUID.randomUUID().toString();
+      System.out.println("Tracking number of Order:" + orderNum + " is " + trackingNum);
+      return Response.ok(new DeliveryStatusDTO(orderNum, trackingNum,
+          "shirts delivery dispatched")).build();
+    }
+
+    @Override
+    public Response retrieve(String orderNum) {
+      if (orderNum.isEmpty()) {
+        return Response.status(Response.Status.BAD_REQUEST).entity("Invalid Order Num").build();
+      }
+      return Response.ok("Order: " + orderNum + " returned").build();
+    }
+  }
+}
